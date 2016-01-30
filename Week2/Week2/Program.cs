0146225bc025ef74c1040e1c4a784b0f0649fcc0
GoYa76KinG <goya76king@gmail.com>
2016-01-30 22:22:24 +0600

    Added Commentaries

--- Week2/Week2/Program.cs
+++ Week2/Week2/Program.cs
@@ -12,17 +12,18 @@ namespace Week2
         static void Main(string[] args)
         {
             int[] array = new int[6];
-            FileStream fsi = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            FileStream fso = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            StreamReader rs = new StreamReader(fsi);
-            StreamWriter sw = new StreamWriter(fso);
-            string[] massive = rs.ReadLine().Split(' ');
-            for (int i = 0; i < massive.Length; i++)
+            FileStream fsi = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite); // путь к файлу чтения
+            FileStream fso = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite); // путь к файлу сохранения
+            StreamReader rs = new StreamReader(fsi); // открываем поток входа данных
+            StreamWriter sw = new StreamWriter(fso); // то же самое с выходом
+            string[] massive = rs.ReadLine().Split(' '); // создаем массив и сплитим по пробелу
+            for (int i = 0; i < massive.Length; i++) // меняем типы данных эдементов массива
                 array[i] = int.Parse(massive[i]);
             int k = 0;
-            int min = array[0];
-            for (int i = 0; i < array.Length; i++)
+            int min = array[0]; // минимум изначально
+            for (int i = 0; i < array.Length; i++) // пробегаемся по всем элементам
             {
+                // далее проверки на простоту и нахождение минимального
                 for (int j = 1; j < min; j++)
                 {
                     if (array[i] % j == 0)
@@ -36,7 +37,7 @@ namespace Week2
                 }
                 k = 0;
             }
-            sw.WriteLine("Minumum prime number is: " + min);
+            sw.WriteLine("Minumum prime number is: " + min); // записываем в файл
             sw.Close();
             rs.Close();
         }
