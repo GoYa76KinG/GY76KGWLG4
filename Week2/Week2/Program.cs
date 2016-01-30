893665a438b621ba9311f42745e8bf19c31a7328
GoYa76KinG <goya76king@gmail.com>
2016-01-30 21:57:03 +0600

    Added Max & Min IO.txt

--- Week2/Week2/Program.cs
+++ Week2/Week2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,30 @@ namespace Week2
     {
         static void Main(string[] args)
         {
+            int[] array = new int[6];
+            FileStream fsi = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream fso = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader rs = new StreamReader(fsi);
+            StreamWriter sw = new StreamWriter(fso);
+            string[] massive = rs.ReadLine().Split(' ');
+            for (int i = 0; i < massive.Length; i++)
+                array[i] = int.Parse(massive[i]);
+            int min = array[0];
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (min > array[i])
+                    min = array[i];
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                    max = array[i];
+            }
+            sw.WriteLine("Minumum is: " + min);
+            sw.WriteLine("Maximum is: " + max);
+            sw.Close();
+            rs.Close();
         }
     }
 }
