f2c6d4b8c731e604993c90bfeb01109a670f5086
GoYa76KinG <goya76king@gmail.com>
2016-01-30 18:54:17 +0600

    Added PrimeNumbers

--- Week1/Week1/Program.cs
+++ Week1/Week1/Program.cs
@@ -10,6 +10,23 @@ namespace Week1
     {
         static void Main(string[] args)
         {
+             Console.WriteLine("\aEnter your numbers: "); 
+             string PrimeNumbers = Console.ReadLine(); 
+             string[] Prime = PrimeNumbers.Split(); 
+             foreach (string num in Prime) 
+             {
+                 int PrimeNumb = int.Parse(num); 
+                 int i, k = 1; 
+                 for (i = 2; (i < PrimeNumb) && (PrimeNumb != 1) && (PrimeNumb != 2) && (k != 0); i++) 
+                 {
+                     k = PrimeNumb % i;
+                 }
+                 if ((k != 0) && (PrimeNumb != 0)) 
+                 {
+                     Console.Write("{0} ", PrimeNumb);
+                 }
+             }
+             Console.ReadKey();
         }
     }
 }
