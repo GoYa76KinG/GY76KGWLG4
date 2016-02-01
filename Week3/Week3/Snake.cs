8af665aed3cd0da9b7300b6d9ee9e6331833c4d3
GoYa76KinG <goya76king@gmail.com>
2016-02-01 22:53:00 +0600

    Motivation & CopyPaste, not for lab

---
+++ Week3/Week3/Snake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Week3
+{
+    public class Snake_fragment
+    {
+        public int x, y;
+
+        public Snake_fragment(int X, int Y)
+        {
+            x = X;
+            y = Y;
+        }
+    }
+}
