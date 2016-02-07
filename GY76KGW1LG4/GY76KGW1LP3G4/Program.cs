b637f1bf87dcc208f54fe422652c2ecc3732db7a
GoYa76KinG <goya76king@gmail.com>
2016-02-07 10:44:34 +0600

    GY76KGW1LG4/Added solution for L1P8

---
+++ GY76KGW1LG4/GY76KGW1LP3G4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GY76KGW1LP3G4
+{
+    class Program
+    {
+        static string Select()
+        {
+            Console.WriteLine("Enter word \"add\" to add new contact");
+            Console.WriteLine("Enter word \"list\" to add new contact");
+            string choise = Console.ReadLine();
+            string selection = choise;
+            return selection;
+        }
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string selection = Select();
+                if (selection == "add")
+                {
+                    Console.WriteLine("\aEnter name of student: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("\aEnter his surname: ");
+                    string surname = Console.ReadLine();
+                    Console.WriteLine("\aEnter his faculty: ");
+                    string faculty = Console.ReadLine();
+                    Console.WriteLine("\aEnter his specialization: ");
+                    string specialization = Console.ReadLine();
+                    Console.WriteLine("\aEnter his course of studying: ");
+                    string Course = Console.ReadLine();
+                    int course = int.Parse(Course);
+                    Console.WriteLine("\aEnter his GPA: ");
+                    string gpa = Console.ReadLine();
+                    double GPA = double.Parse(gpa);
+                    Console.WriteLine(Student.add(name, surname, faculty, specialization, course, GPA));
+                }
+                if (selection == "list") 
+                {
+                    for (int i = 0; i < Student.k; i++) 
+                    {
+                        Console.WriteLine(Student.list[i]); 
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GY76KGW1LG4/GY76KGW1LP3G4/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GY76KGW1LP3G4
+{
+    public class Student
+    {
+        public static Student[] list = new Student[100];
+        public string name;
+        public string surname;
+        public string faculty;
+        public string specialization;
+        public int course;
+        public double GPA;
+        public Student(string name, string surname, string faculty, string specialization, int course, double GPA)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.faculty = faculty;
+            this.specialization = specialization;
+            this.course = course;
+            this.GPA = GPA;
+        }
+        public static int k = 0;
+        public static Student add(string name, string surname, string faculty, string specialization, int course, double GPA)
+        {
+            Student student = new Student(name, surname, faculty, specialization, course, GPA);
+            list[k] = student;
+            k++;
+            return student;
+        }
+        public override string ToString()
+        {
+            return "Name of student: " + this.name + "\nSurname of student: " + this.surname + "\nFaculty of student: " + this.faculty + "\nSpecialization =: " + this.specialization + "\nCourse of studying: " + this.course + "\nStudent's GPA: " + this.GPA;
+        }
+    }
+}
